2f6802e3307b52e60a93f093368c3d6c36acf75d
jana_j <jana.jankovic@student.um.si>
2021-01-17 17:59:09 +0100

    update ip

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -1,6 +1,6 @@
 export class GlobalConstants {
 
-  public static apiURL: string = "http://193.19.220.59:";
-  public static port: string = "3001";
+  public static apiURL: string = "http://92.242.251.240";
+  public static port: string = ":3001";
 
 }
